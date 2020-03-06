a7c77308fb143be632e910e64ba95961f55626a9
Hiroshi Fujiwara <fujiwara@no1s.biz>
2020-03-06 12:30:06 +0000

    added for complete

---
+++ 03-platformer/assets/data/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="tiles" tilewidth="70" tileheight="70" tilecount="12" columns="4">
+ <image source="tiles.png" trans="ffffff" width="280" height="210"/>
+</tileset>
